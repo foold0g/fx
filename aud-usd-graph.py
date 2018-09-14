6d074c5311e82f212e3edf59f40375b8322452b1
Suman Maparu <maparus@ryanair.com>
2018-09-14 17:00:50 +0100

    Improved the date

--- aud-usd-graph.py
+++ aud-usd-graph.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("audusd.txt", sep=None, delimiter="\t", usecols=[0, 1], header=None)
 np_data = np.array(data)
-plt.plot(np_data[:,0], np_data[:,1])
+np_dates = [dt.datetime.strptime(x, '%d-%b-%Y').date() for x in np_data[:,0]]
+plt.plot(np_dates, np_data[:,1])
